e9aeafbbc9c3079104db97b11100c0a827152554
Philipp Immel <philipp.immel@gmail.com>
2020-05-18 12:20:49 +0200

    Calculate page numbers for bookmarks

--- cambridgecorepdf.sh
+++ cambridgecorepdf.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+shopt -s nullglob
 
 # Cambridge Core PDF
 # Merges and Bookmarks PDFs from Cambridge Core
 #
-# Dependencies: pdftk
+# Dependencies: pdftk awk
 
 # Unzip the archive
 
 # Merge files
 pdftk *.pdf cat output merged.pdf
+
+# Extract section titles from file names
+
+# Extract page numbers from file names
+
+# Calculate page numbers for bookmarks
+pageno=1
+for file in *.pdf; do
+    echo $pageno
+    pageno=$(($pageno+$(pdftk $file dump_data | awk '/'NumberOfPages'/ {print $2}')))
+done
