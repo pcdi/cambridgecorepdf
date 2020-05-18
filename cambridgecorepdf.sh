9de45b45b0d972d600b5c840a66cfa6a0af570dd
Philipp Immel <philipp.immel@gmail.com>
2020-05-18 12:54:17 +0200

    Write bookmarks file for pdftk

--- cambridgecorepdf.sh
+++ cambridgecorepdf.sh
@@ -9,14 +9,21 @@ shopt -s nullglob
 # Unzip the archive
 
 # Merge files
-# pdftk *.pdf cat output merged.pdf
+pdftk *.pdf cat output merged.pdf
 
+# Make bookmarks
+BMFILE=bookmarks.txt
 pageno=1
+
 for file in *.pdf; do
+    echo "BookmarkBegin" >> $BMFILE
+    
     # Extract section titles from file names
     sectiontitle=$(echo $file | cut -d'_' -f5- | sed s/'_'/' '/g | sed s/'\.pdf'//g)
-    echo $sectiontitle
+    echo "BookmarkTitle: " $sectiontitle >> $BMFILE
+    echo "BookmarkLevel: 1" >> $BMFILE
+    
     # Calculate page numbers for bookmarks
-    echo $pageno
+    echo "BookmarkPageNumber: " $pageno >> $BMFILE
     pageno=$(($pageno+$(pdftk $file dump_data | awk '/'NumberOfPages'/ {print $2}')))
 done
